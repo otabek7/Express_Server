7161d0f7d880793ec982033a1901c3a8989963f7
Otabek <mavlonovo@tamu.edu>
2023-08-03 22:53:29 -0500

    added get

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ import express from "express"; /* imports express */
 const app = express(); /* create an app using the express object */
 const port = 3000;
 
+app.get("/", (req, res) => {
+  res.send("Hello, World!");
+});
+
 app.listen(port, () => { /* use the listen method and specify 2 things. The port (location of server) and the callback function that will be triggered when our server is setup */
   console.log(`Server running on port ${port}.`); /* just log where the server is running */
 }
